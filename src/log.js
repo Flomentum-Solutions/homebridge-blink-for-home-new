b233349a3549f3413eec7eceb0447384a6f506c7
Matt May <m@mrmsquared.com>
2025-11-01 11:36:33 -0400

    fix log.warn error

--- src/log.js
+++ src/log.js
@@ -1,6 +1,7 @@
 let log = (...data) => console.log(...data);
 log.info = (...data) => console.info(...data);
 log.debug = (...data) => console.debug(...data);
+log.warn = (...data) => console.warn(...data);
 log.error = (...data) => console.error(...data);
 
 function setLogger(logger, verbose = false, debug = false) {
@@ -8,11 +9,15 @@ function setLogger(logger, verbose = false, debug = false) {
 
     if (logger.debugEnabled) {
         log = logger;
+        if (typeof log.warn !== 'function') {
+            log.warn = (...data) => log.info(...data);
+        }
     }
     else {
         log = (...data) => logger(...data);
         log.error = (...data) => logger.error(...data);
         log.info = () => undefined;
+        log.warn = (...data) => (logger.warn ? logger.warn(...data) : logger(...data));
         log.debug = () => undefined;
         if (verbose || debug) {
             log.info = (...data) => logger.info(...data);
